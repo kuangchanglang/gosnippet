91ed5901f1a3f0ba6d1f09ef24117b19cac389cf
kuangchanglang <kuangchanglang@163.com>
2016-06-27 16:09:16 +0800

    refactoring

--- util/struct.go
+++ util/struct.go
@@ -5,6 +5,12 @@ import (
 	"sort"
 )
 
+// TagField represents field that with tag in struct
+type TagField struct {
+	Tag   string
+	Field reflect.Value
+}
+
 // GetFieldsSortedByTag returns tags and its' corresponding values sorted
 // by given tagKey in alphabet order.
 // (nil, nil) is return if v is not struct
@@ -17,35 +23,39 @@ import (
 //          Age    int    `schema:"age"`
 //    }
 //    u := User{"0000", "alice", 20}
-//    tags, values := GetFieldsSortedByTag(u, "schema")
-//    // tags=["age", "name", "passwd"], values=[20, "alice", "0000]
-//    GetFieldsSortedByTag("string") // nil, nil is return
+//    fields := GetFieldsSortedByTag(u, "schema")
+//    // fields =[{"age", 20}, {"name", "alice"}, {"passwd", "0000"}]
+//    GetFieldsSortedByTag("string") // nil is return
 //
-func GetFieldsSortedByTag(v interface{}, tagKey string) (tags []string, values []reflect.Value) {
+func GetFieldsSortedByTag(v interface{}, tagKey string) (fields []TagField) {
 	rt := reflect.TypeOf(v)
 	if rt.Kind() != reflect.Struct { // type not struct
-		return nil, nil
+		return nil
 	}
 
-	tags = make([]string, 0)          // tags, for sorting
-	fields := make(map[string]string) // key: tag, value: field name
+	tags := make([]string, 0)           // tags, for sorting
+	fieldMap := make(map[string]string) // key: tag, value: field name
 	for i := 0; i < rt.NumField(); i++ {
 		f := rt.Field(i)
 		t := f.Tag.Get(tagKey)
 		if t == "" { // escape empty tag
 			continue
 		}
-		fields[t] = f.Name
+		fieldMap[t] = f.Name
 		tags = append(tags, t)
 	}
 	sort.Strings(tags)
 
 	value := reflect.ValueOf(v)
-	values = make([]reflect.Value, 0)
+	fields = make([]TagField, 0)
 	for _, t := range tags {
-		fieldName := fields[t]
-		values = append(values, value.FieldByName(fieldName))
+		fieldName := fieldMap[t]
+		f := TagField{
+			Tag:   t,
+			Field: value.FieldByName(fieldName),
+		}
+		fields = append(fields, f)
 	}
 
-	return tags, values
+	return fields
 }
--- util/struct_test.go
+++ util/struct_test.go
@@ -2,7 +2,6 @@ package util
 
 import (
 	"fmt"
-	"reflect"
 	"testing"
 )
 
@@ -15,58 +14,50 @@ type User struct {
 }
 
 func TestGetFieldSortedByTag(t *testing.T) {
-	tags, values := GetFieldsSortedByTag(1, "schema") // int
-	if tags != nil {
-		t.Errorf("tags %v!=nil", tags)
-		return
-	}
-	if values != nil {
-		t.Errorf("values %v!=nil", values)
+	fields := GetFieldsSortedByTag(1, "schema") // int
+	if fields != nil {
+		t.Errorf("tags %v!=nil", fields)
 		return
 	}
 
-	tags, values = GetFieldsSortedByTag("string", "schema") // string
-	if tags != nil {
-		t.Errorf("tags %v!=nil", tags)
-		return
-	}
-	if values != nil {
-		t.Errorf("values %v!=nil", values)
+	fields = GetFieldsSortedByTag("string", "schema") // string
+	if fields != nil {
+		t.Errorf("tags %v!=nil", fields)
 		return
 	}
 
 	u := User{"kcl", "123", "a", "c", "b"}
 
-	tags, values = GetFieldsSortedByTag(u, "schema")
-	fmt.Printf("tags: %v, values: %v\n", tags, values)
+	fields = GetFieldsSortedByTag(u, "schema")
+	fmt.Printf("fields: %v\n", fields)
 
 	expTags := []string{"a", "b", "name", "password"}
 	expValues := []string{"a", "b", "kcl", "123"}
 
-	if reflect.DeepEqual(tags, expTags) == false {
-		t.Errorf("expect: %v, got: %v", expTags, tags)
-		return
-	}
-	for i, _ := range values {
-		if values[i].String() != expValues[i] {
-			t.Errorf("expect: %v, got: %v", values, expValues)
+	for i, f := range fields {
+		if f.Tag != expTags[i] {
+			t.Errorf("expect: %v, got: %v", f.Tag, expTags[i])
+			return
+		}
+		if f.Field.String() != expValues[i] {
+			t.Errorf("expect: %v, got: %v", f.Field.String(), expValues[i])
 			return
 		}
 	}
 
-	tags, values = GetFieldsSortedByTag(u, "json")
-	fmt.Printf("tags: %v, values: %v\n", tags, values)
+	fields = GetFieldsSortedByTag(u, "json")
+	fmt.Printf("fields: %v\n", fields)
 
 	expTags = []string{"1", "2", "3", "4"}
 	expValues = []string{"c", "123", "kcl", "a"}
 
-	if reflect.DeepEqual(tags, expTags) == false {
-		t.Errorf("expect: %v, got: %v", expTags, tags)
-		return
-	}
-	for i, _ := range values {
-		if values[i].String() != expValues[i] {
-			t.Errorf("expect: %v, got: %v", values, expValues)
+	for i, f := range fields {
+		if f.Tag != expTags[i] {
+			t.Errorf("expect: %v, got: %v", f.Tag, expTags[i])
+			return
+		}
+		if f.Field.String() != expValues[i] {
+			t.Errorf("expect: %v, got: %v", f.Field.String(), expValues[i])
 			return
 		}
 	}
